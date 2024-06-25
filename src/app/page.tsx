e806ea9622c0944e36f154b1b0b7d02112057e52
Maylon-tech <minatokiroisen77@gmail.com>
2024-06-25 12:12:24 +0900

    add component of products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,35 @@
 
-export default function Home() {
+import { ProductType } from "@/types/productType"
+import Product from "./components/Product"
+
+async function getProducts() {
+  const res = await fetch('https://fakestoreapi.com/products')
+  if(!res.ok) {
+    throw new Error("Failed to Fetch data")
+  }
+  return res.json()
+}
+
+
+
+export default async function Home() {
+  const products = await getProducts()
+
+  // fake store Products API  --  https://fakestoreapi.com/products
+
   return (
-      <main>
-        <h1>Next Ecommerce 13</h1>
-      </main>
+      <div className="max-w-7xl mx- auto pt-8 px-8 xl:px-0">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:gap-6">
+
+          {
+            products.map((product: ProductType) => (
+              <Product 
+                key={product.id}
+                product={product}
+              />
+            ))
+          }          
+        </div>
+      </div>
   )
 }
