96a2e8b283baf74167195b5f1e943b6e69d5a6f8
Maylon-tech <minatokiroisen77@gmail.com>
2024-06-26 11:35:17 +0900

    styling the components products with Next Image

--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -1,5 +1,5 @@
-
 import { ProductType } from '@/types/productType'
+import ProductImage from './ProductImage'
 
 type ProductProps = {
     product: ProductType
@@ -10,11 +10,15 @@ export default function Product({ product }: ProductProps) {
         <div className="flex flex-col shadow-lg h-96 bg-slate-800 p-5 text-gray-300">
 
             <div className='relative max-h-72 flex-1'>
-                IMG
+                <ProductImage product={product} fill />
             </div>
-            <div className='flex justify-between font-bold my-3
-            '>
-                {product.title}
+            <div className='flex justify-between font-bold my-3'>
+                <p className="w-40 truncate">
+                    {product.title}
+                </p>
+                <p className="text-md text-teal-300">
+                    {product.price}
+                </p> 
             </div>
 
             <button
